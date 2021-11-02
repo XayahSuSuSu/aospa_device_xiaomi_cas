87c5ebdd82f44a644d4489398c9faa62ac70a6f3
Xayah <zds1249475336@gmail.com>
2021-11-02 19:15:02 +0800

    cas: Drop telephony

--- device/xiaomi/cas/device.mk
+++ device/xiaomi/cas/device.mk
@@ -362,19 +362,6 @@ PRODUCT_PACKAGES += \
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
-# Telephony
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
