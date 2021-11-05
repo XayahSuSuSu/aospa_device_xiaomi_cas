4cd3d38b01837f32a336fd863d413006617b2f0b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-05 23:11:57 +0800

    cas: Don't override DEVICE_{MATRIX|MANIFEST}_FILE
    * Allow device-specific manifests to be safely added
    
    Change-Id: Iccafe8c8491219336389015af8caa2c7a700ff61

--- device/xiaomi/cas/BoardConfig.mk
+++ device/xiaomi/cas/BoardConfig.mk
@@ -82,7 +82,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/vintf/device_framework_matrix.xml \
     $(DEVICE_PATH)/vintf/xiaomi_framework_matrix.xml \
 
-DEVICE_MANIFEST_FILE := \
+DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/vintf/manifest.xml \
     $(DEVICE_PATH)/vintf/xiaomi_manifest.xml
 
