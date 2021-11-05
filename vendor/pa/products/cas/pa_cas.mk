51fe89ae15723c40287911a21c9aa5c017c28068
Xayah <zds1249475336@gmail.com>
2021-11-05 20:34:41 +0800

    cas: Stay Paranoid

--- vendor/pa/products/cas/pa_cas.mk
+++ vendor/pa/products/cas/pa_cas.mk
@@ -4,17 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Check for target product
+ifeq (pa_cas,$(TARGET_PRODUCT))
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Include Paranoid Android common configuration
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
-# Inherit from the ArrowOS configuration.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common PA configuration
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_cas
+# Override build properties
+PRODUCT_NAME := pa_cas
 PRODUCT_DEVICE := cas
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
@@ -25,3 +32,5 @@ DEVICE_MAINTAINER := Xayah
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Xiaomi/cas/cas:11/RKQ1.200826.002/V12.5.6.0.RJJCNXM:user/release-keys
+
+endif
