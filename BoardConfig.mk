bd54b8f3b61379ae4d2d44dc9ad0e249758c34cf
Mesquita <mesquita@aospa.co>
2022-03-25 15:06:07 +0800

    cas: Drop fwk detect props
     * Already defined in pa build
    
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,7 +156,6 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
